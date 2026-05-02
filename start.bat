b2df7d4a52146343c1294eb08397505cebd8624e
RONAKJAIN122 <jain.ronak2007@gmail.com>
2026-05-03 03:23:39 +0530

    added start.bat to make it 1 click

---
+++ start.bat
@@ -0,0 +1,12 @@
+@echo off
+title SplitPay Server
+echo.
+echo  ==========================================
+echo   SplitPay - Starting Local Server...
+echo  ==========================================
+echo.
+echo  Once started, open your browser and go to:
+echo  http://localhost:5000
+echo.
+npm start
+pause
